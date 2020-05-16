e6923a1ff48afe280301962eede76a1c22e3be4a
Nelson <ong.nelson4@gmail.com>
2020-05-16 08:29:17 -0400

    more intermediate learnings

---
+++ Python_Projects/Intermediate/requeststests.py
@@ -0,0 +1,12 @@
+import requests
+
+params = {'#q':'pizza'}
+
+r = requests.get('https://bing.com/search', params=params)
+print('Status:', r.status_code)
+
+print(r.url)
+
+f = open('./page.html', 'w+')
+
+f.write(r.text)
